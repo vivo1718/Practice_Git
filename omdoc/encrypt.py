97d4718d23ccb5885311e4e255b2ac5ed1b9d64f
axongit2021 <sweetytiwaripaa92@gmail.com>
2021-05-15 21:43:52 +0530

    ok

---
+++ omdoc/encrypt.py
@@ -0,0 +1,25 @@
+
+message = input("Enter the message")
+key = int(input("Enter the key value"))
+
+
+def encrypt(message,key):
+       cypher = ""
+       for i in message:
+              if i.isupper():
+                     cypher += chr((ord(i)+key)%26 +65)
+              elif i.islower():
+                     cypher += chr((ord(i)+key)%26 +97)
+              else:
+                     cypher +=""
+       return cypher
+print("encrypted message is:",encrypt(message,key))
+
+ec = encrypt(message,key)
+print(ec)
+                     
+
+
+
+
+
